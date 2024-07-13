012095ef47168879297676739b1e4bc032e63aa6
Carlos-Cuestas <125101203+Carlos-Cuestas@users.noreply.github.com>
2024-07-12 23:36:12 -0600

    Paso 1 al 4

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,55 @@
 import styles from "./page.module.css";
-/*const element = <h1>Hola, Mundo!</h1>;Paso 4*/
-const element = (
-  <>
-    <h1>Hola, Mundo!</h1>
-    <h2>Son las {new Date().toTimeString()}</h2>
-  </>
-);
+const Equipos = ({ equipos }) => {
+  return (
+    <div className={styles.container__list}>
+      <h2 className={styles.title}>Equipos de Fútbol</h2>
+      {equipos.map((equipo) => (
+        <div key={equipo.id}>
+          <h3 className={styles.nameclub}>{equipo.nombre}</h3>
+          <ul>
+            {equipo.plantilla.map((jugador) => (
+              <li className={styles.container__list} key={jugador.id}>
+                <strong>{jugador.nombre}</strong>
+                <p>
+                  Altura: {jugador.Altura}m
+                  <br></br>
+                  {jugador.Peso}Kg
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
+
 export default function Home() {
+  const equiposData = [
+    {
+      "id": 1,
+      "nombre": "Real Madrid",
+      "plantilla": [
+        { "id": 1, "nombre": "Eden Hazard", "Altura": "1.75", "Peso": "74Kg" },
+        { "id": 2, "nombre": "Gonzalo García", "Altura": "1.82", "Peso": "74Kg" },
+        { "id": 3, "nombre": "Karim Benzema", "Altura": "1.85", "Peso": "81Kg" }
+      ]
+    },
+    {
+      "id": 2,
+      "nombre": "Barcelona",
+      "plantilla": [
+        { "id": 1, "nombre": "Marc-André ter Stegen", "Altura": "1.75", "Peso": "74Kg" },
+        { "id": 2, "nombre": "Iñigo Martinez", "Altura": "1.82", "Peso": "74Kg" },
+        { "id": 3, "nombre": "Gavi", "Altura": "1.85", "Peso": "81Kg" }
+      ]
+    }
+  ];
   return (
     <main className={styles.main}>
-      <div className="App">
-        {element}
+      <div>
+        <h1>Mi Aplicación de Fútbol</h1>
+        <Equipos equipos={equiposData} />
       </div>
     </main>
   );
